6a997612dda535e464473ad67d25b924308cd10a
ipeter50 <pierre.le-jeune@live.fr>
2019-02-08 16:29:46 +0100

    Update h

--- warmup/searchclient/searchclient_python/searchclient/heuristic.py
+++ warmup/searchclient/searchclient_python/searchclient/heuristic.py
@@ -5,6 +5,7 @@ class Heuristic(metaclass=ABCMeta):
     def __init__(self, initial_state: 'State'):
         self.goals_list = {}
         # self.boxes_list = {}
+        self.nb_goals = 0
 
 
         for row in range(len(initial_state.goals)-1):
@@ -13,7 +14,8 @@ class Heuristic(metaclass=ABCMeta):
                 if initial_state.goals[row][col]:
                     if not initial_state.goals[row][col] in self.goals_list:
                         self.goals_list[initial_state.goals[row][col]] = []
-                    self.goals_list[initial_state.goals[row][col]].append((row,col))
+                    self.goals_list[initial_state.goals[row][col]].append((row,col, self.nb_goals * 200))
+                    self.nb_goals += 1
                 # if initial_state.boxes[row][col]:
                 #     if self.boxes_list[initial_state.boxes[row][col]] == None:
                 #         self.boxes_list[initial_state.boxes[row][col]] = []
@@ -23,8 +25,14 @@ class Heuristic(metaclass=ABCMeta):
     def h(self, state: 'State') -> 'int':
         res = 0
         current_boxes = {}
-
+        agent = (state.agent_row, state.agent_col)
         man_dist = lambda a,b : abs(a[0]-b[0]) + abs(a[1]-b[1])
+
+        def subH(agent, goal, box):
+            if man_dist(box, goal) == 0:
+                return 0
+            return man_dist(box, goal) + man_dist(agent, box) + goal[2]
+
         # file = open("log.txt", 'w')
         for row in range(len(state.goals)):
             for col in range(len(state.goals[row])):
@@ -39,13 +47,16 @@ class Heuristic(metaclass=ABCMeta):
         for goals_name in self.goals_list.keys():
             for goal in self.goals_list[goals_name]:
                 try:
-                    res += min(man_dist(goal,box) for box in current_boxes[goals_name.upper()])
+                    res += min(subH(agent,goal,box) for box in current_boxes[goals_name.upper()])
+
                 except:
                     res = 0
 
         # file = open("log.txt", 'w')
         # file.write(str(current_boxes))
         return res
+
+
 
     @abstractmethod
     def f(self, state: 'State') -> 'int': pass
